e6fd41332c7f9b4a0c7efa87263782853197939b
Christopher Reutz <chrisreutz@yahoo.com>
2019-04-01 22:36:42 -0700

    completed flask app

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Unit 10 Assignment - Surf's Up!
 
 By Christopher Reutz
+
+The "Step 1 - Climate Analysis and Exploration" is contained in climate_analysis.ipynb
+
+The "Step 2 - Climate App" is contained in climate_app.py
--- climate_app.py
+++ climate_app.py
@@ -32,6 +32,12 @@ Station = Base.classes.station
 # Create our session (link) from Python to the DB
 session = scoped_session(sessionmaker(bind=engine))
 
+# Calculate the date 1 year ago from the last data point in the database
+lastdate = session.query(Measurement.date).\
+    order_by(Measurement.date.desc()).first()
+lastdate = dt.datetime.strptime(lastdate[0], '%Y-%m-%d').date()
+yearago = lastdate - dt.timedelta(days=365)
+
 #################################################
 # Flask Setup
 #################################################
@@ -41,7 +47,6 @@ app.config["JSON_SORT_KEYS"] = False
 #################################################
 # Flask Routes
 #################################################
-
 @app.route("/")
 def home():
     """List all available api routes."""
@@ -54,16 +59,11 @@ def home():
         f"/api/v1.0/&lt;start&gt;/&lt;end&gt;<br/>"
     )
 
+#################################################
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """Return last 12 months of precipitation data"""
-    # Calculate the date 1 year ago from the last data point in the database
-    lastdate = session.query(Measurement.date).\
-        order_by(Measurement.date.desc()).first()
-    lastdate = dt.datetime.strptime(lastdate[0], '%Y-%m-%d').date()
-    yearago = lastdate - dt.timedelta(days=365)
-
-    # Perform a query to retrieve the data and precipitation scores
+    # Perform a query to retrieve the date and precipitation scores
     results = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= yearago).\
         order_by(Measurement.date.asc()).all()
@@ -74,9 +74,10 @@ def precipitation():
         prcp_dict = {}
         prcp_dict[date] = prcp
         prcp_data.append(prcp_dict)
-
-    return jsonify(prcp_data)    
     
+    return jsonify(prcp_data)    
+
+#################################################  
 @app.route("/api/v1.0/stations")
 def stations():
     """Return a list of stations"""
@@ -98,6 +99,78 @@ def stations():
     
     return jsonify(stn_data)
 
+#################################################
+@app.route("/api/v1.0/tobs")
+def tempobs():
+    """Return temperature observations for the previous year"""
+    # Perform a query to retrieve the temperature observations
+    results = session.query(Measurement.date,\
+        Measurement.station, Measurement.tobs).\
+        filter(Measurement.date >= yearago).\
+        order_by(Measurement.date.asc()).all()
+    
+    # Create a dictionary from the row data and append to a list of temp_data
+    temp_data = []
+    for date, station, tobs in results:
+        temp_dict = {}
+        temp_dict["date"] =  date
+        temp_dict["station"] = station
+        temp_dict["tobs"] = tobs
+        temp_data.append(temp_dict)
+    
+    return jsonify(temp_data)
+
+#################################################    
+@app.route("/api/v1.0/<string:start>")
+def tempstats_startonly(start):
+    """Return TMIN, TAVG, and TMAX from a start date and after"""
+    # Perform a query to retrieve the temperature statistics
+    startdate = dt.datetime.strptime(start, '%Y-%m-%d').date()
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),\
+        func.max(Measurement.tobs)).\
+        filter(Measurement.date >= startdate).all()
+    
+    # Create a dictionary from the row data and append to a list of temp_data
+    temp_data = []
+    for tmin, tavg, tmax in results:
+        temp_dict = {}
+        temp_dict["TMIN"] = tmin
+        temp_dict["TAVG"] = tavg
+        temp_dict["TMAX"] = tmax
+        temp_data.append(temp_dict)
+
+    return jsonify(temp_data)
+
+################################################# 
+@app.route("/api/v1.0/<string:start>/<string:end>")
+def tempstats_startend(start, end):
+    """Return TMIN, TAVG, and TMAX from a start date to end date"""
+    
+    # Check to see if end date occurs on or after the start date
+    if end < start:
+        return "The end date must be on or after the start date."
+    
+    else:    
+        # Perform a query to retrieve the temperature statistics
+        startdate = dt.datetime.strptime(start, '%Y-%m-%d').date()
+        enddate = dt.datetime.strptime(end, '%Y-%m-%d').date()
+        results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),\
+            func.max(Measurement.tobs)).\
+            filter(Measurement.date >= startdate).\
+            filter(Measurement.date <= enddate).all()
+    
+        # Create a dictionary from the row data and append to a list of temp_data
+        temp_data = []
+        for tmin, tavg, tmax in results:
+            temp_dict = {}
+            temp_dict["TMIN"] = tmin
+            temp_dict["TAVG"] = tavg
+            temp_dict["TMAX"] = tmax
+            temp_data.append(temp_dict)
+
+        return jsonify(temp_data)    
+
+#################################################
 @app.teardown_request
 def remove_session(ex=None):
     session.remove()
